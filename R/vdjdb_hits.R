fe11f1dac9ea5af98b975bc2e9c87283f1be8242
Christopher Skopnik <christopher.skopnik@cohenfano.local>
2021-12-03 19:10:10 +0100

    up

--- R/vdjdb_hits.R
+++ R/vdjdb_hits.R
@@ -12,26 +12,49 @@ vdj_hits <- function(vdjdb,
                      vdj_tr_col = "Gene",
                      tcr_tr_col = "chain",
                      vdj_cdr3_col = "CDR3",
-                     tcr_cdr3_col = "CDR3_aa_cr") {
+                     tcr_cdr3_col = "CDR3_aa_cr",
+                     separate_TRA_TRB_only = T,
+                     ...) { # ... stringdist nthread
 
-  # ... stringdist nthread
-  tcrs <-
-    cl_long[,which(names(cl_long) %in% c("chain", "CDR3_aa_cr"))] %>%
-    dplyr::distinct()
+  if (vdj_tr_col == tcr_tr_col) {
+    stop("Please avoid same names for tcr_tr_col and vdj_tr_col.")
+  }
+  if (vdj_cdr3_col == tcr_cdr3_col) {
+    stop("Please avoid same names for tcr_cdr3_col and vdj_cdr3_col")
+  }
 
+
+  tcrs <- cl_long
+  tcrs <- dplyr::distinct(tcrs, !!sym(tcr_tr_col), !!sym(tcr_cdr3_col))
+
+#/Volumes/AG_Hiepe/Christopher.Skopnik/2019_scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv
+  #"/Users/vonskopnik/Documents/scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv"
   vdjdb <-
-    read.csv("/Users/vonskopnik/Documents/scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv", sep = "\t", header = T) %>%
+    read.csv("/Volumes/AG_Hiepe/Christopher.Skopnik/2019_scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv", sep = "\t", header = T) %>%
     dplyr::filter(Species == "HomoSapiens") %>%
-    dplyr::distinct(CDR3, Gene)
-
-  # Biostrings pDict approach and similar are not applicable
+    dplyr::distinct(!!sym(vdj_cdr3_col), !!sym(vdj_tr_col))
 
   vdjdb <- vdjdb %>% dplyr::group_by(Gene) %>% dplyr::slice(1:5)
   tcrs <- tcrs %>% dplyr::group_by(chain) %>% dplyr::slice(1:5)
-  matches <- mapply(stringdist::stringdistmatrix, split(vdjdb$CDR3, vdjdb$Gene), split(tcrs$CDR3_aa_cr, tcrs$chain), method = "lv", useNames = "strings", SIMPLIFY = F)
-  matches <- sapply(matches, reshape2::melt, simplify = F, c("CDR3", "CDR3_aa_cr"), value.name = "lv")
-  matches <- do.call(rbind, matches)
-  matches$TR <- sapply(strsplit(rownames(matches), "\\."), "[", 1)
+  if (separate_TRA_TRB_only) {
+    matches <- mapply(stringdist::stringdistmatrix,
+                      split(vdjdb$CDR3, vdjdb$Gene)[c("TRA", "TRB")],
+                      split(tcrs$CDR3_aa_cr, tcrs$chain)[c("TRA", "TRB")],
+                      method = "lv",
+                      useNames = "strings",
+                      SIMPLIFY = F,
+                      USE.NAMES = T)
+    matches <- sapply(matches, reshape2::melt, simplify = F, c("CDR3", "CDR3_aa_cr"), value.name = "lv")
+    matches <- do.call(rbind, matches)
+    matches[,vdj_tr_col] <- sapply(strsplit(rownames(matches), "\\."), "[", 1)
+    matches[,tcr_tr_col] <- sapply(strsplit(rownames(matches), "\\."), "[", 1)
+  } else {
+
+  }
+
+  split(vdjdb$CDR3, vdjdb$Gene)
+
+
   rownames(matches) <- NULL
 
 
