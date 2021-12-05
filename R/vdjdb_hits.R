1a358fbd4827b89e1fe6f2cba50989e648e16930
Close-your-eyes <vonskopnik@pm.me>
2021-12-05 10:37:31 +0100

    vdj

--- R/vdjdb_hits.R
+++ R/vdjdb_hits.R
@@ -13,7 +13,7 @@ vdj_hits <- function(vdjdb,
                      tcr_tr_col = "chain",
                      vdj_cdr3_col = "CDR3",
                      tcr_cdr3_col = "CDR3_aa_cr",
-                     separate_TRA_TRB_only = T,
+                     TRAxTRB = T,
                      ...) { # ... stringdist nthread
 
   if (vdj_tr_col == tcr_tr_col) {
@@ -30,32 +30,50 @@ vdj_hits <- function(vdjdb,
 #/Volumes/AG_Hiepe/Christopher.Skopnik/2019_scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv
   #"/Users/vonskopnik/Documents/scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv"
   vdjdb <-
-    read.csv("/Volumes/AG_Hiepe/Christopher.Skopnik/2019_scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv", sep = "\t", header = T) %>%
+    read.csv("/Users/vonskopnik/Documents/scRNAseq/R_scRNAseq/2019_SLE_LN/data/20211020_vdjdb.tsv", sep = "\t", header = T) %>%
     dplyr::filter(Species == "HomoSapiens") %>%
     dplyr::distinct(!!sym(vdj_cdr3_col), !!sym(vdj_tr_col))
 
-  vdjdb <- vdjdb %>% dplyr::group_by(Gene) %>% dplyr::slice(1:5)
-  tcrs <- tcrs %>% dplyr::group_by(chain) %>% dplyr::slice(1:5)
-  if (separate_TRA_TRB_only) {
-    matches <- mapply(stringdist::stringdistmatrix,
-                      split(vdjdb$CDR3, vdjdb$Gene)[c("TRA", "TRB")],
-                      split(tcrs$CDR3_aa_cr, tcrs$chain)[c("TRA", "TRB")],
-                      method = "lv",
-                      useNames = "strings",
-                      SIMPLIFY = F,
-                      USE.NAMES = T)
-    matches <- sapply(matches, reshape2::melt, simplify = F, c("CDR3", "CDR3_aa_cr"), value.name = "lv")
-    matches <- do.call(rbind, matches)
-    matches[,vdj_tr_col] <- sapply(strsplit(rownames(matches), "\\."), "[", 1)
-    matches[,tcr_tr_col] <- sapply(strsplit(rownames(matches), "\\."), "[", 1)
-  } else {
+  vdjdb <- vdjdb %>% dplyr::group_by(Gene) %>% dplyr::slice(1:10)
+  tcrs <- tcrs %>% dplyr::group_by(chain) %>% dplyr::slice(1:10)
 
+  if (!identical(sort(unique(vdjdb[,vdj_tr_col,drop=T])), c("TRA", "TRB"))) {
+    stop(paste0(vdj_tr_col, " column of vdjdb should only contain TRA and/or TRB."))
+  }
+  if (!identical(sort(unique(tcrs[,tcr_tr_col,drop=T])), c("TRA", "TRB"))) {
+    stop(paste0(tcr_tr_col, " column of tcrs should only contain TRA and/or TRB."))
   }
 
-  split(vdjdb$CDR3, vdjdb$Gene)
-
-
+  matches <- mapply(stringdist::stringdistmatrix,
+                    split(vdjdb[,vdj_cdr3_col,drop=T], vdjdb[,vdj_tr_col,drop=T])[sort(unique(vdjdb[,vdj_tr_col,drop=T]))],
+                    split(tcrs[,tcr_cdr3_col,drop=T], tcrs[,tcr_tr_col,drop=T])[sort(unique(tcrs[,tcr_tr_col,drop=T]))],
+                    method = "lv",
+                    useNames = "strings",
+                    SIMPLIFY = F)
+  names(matches) <- paste(sort(unique(vdjdb[,vdj_tr_col,drop=T])), sort(unique(tcrs[,tcr_tr_col,drop=T])), sep = "_")
+  matches <- sapply(matches, reshape2::melt, simplify = F, c(vdj_cdr3_col, tcr_cdr3_col), value.name = "lv")
+  matches <- do.call(rbind, matches)
+  matches[,vdj_tr_col] <- sapply(strsplit(sapply(strsplit(rownames(matches), "\\."), "[", 1), "_"), "[", 1)
+  matches[,tcr_tr_col] <- sapply(strsplit(sapply(strsplit(rownames(matches), "\\."), "[", 1), "_"), "[", 2)
   rownames(matches) <- NULL
+
+  if (TRAxTRB) {
+    matches2 <- mapply(stringdist::stringdistmatrix,
+                      split(vdjdb[,vdj_cdr3_col,drop=T], vdjdb[,vdj_tr_col,drop=T])[rev(sort(unique(vdjdb[,vdj_tr_col,drop=T])))],
+                      split(tcrs[,tcr_cdr3_col,drop=T], tcrs[,tcr_tr_col,drop=T])[sort(unique(tcrs[,tcr_tr_col,drop=T]))],
+                      method = "lv",
+                      useNames = "strings",
+                      SIMPLIFY = F)
+    names(matches2) <- paste(rev(sort(unique(vdjdb[,vdj_tr_col,drop=T]))), sort(unique(tcrs[,tcr_tr_col,drop=T])), sep = "_")
+    matches2 <- sapply(matches2, reshape2::melt, simplify = F, c(vdj_cdr3_col, tcr_cdr3_col), value.name = "lv")
+    matches2 <- do.call(rbind, matches2)
+    matches2[,vdj_tr_col] <- sapply(strsplit(sapply(strsplit(rownames(matches2), "\\."), "[", 1), "_"), "[", 1)
+    matches2[,tcr_tr_col] <- sapply(strsplit(sapply(strsplit(rownames(matches2), "\\."), "[", 1), "_"), "[", 2)
+    rownames(matches2) <- NULL
+    matches <- rbind(matches, matches2)
+  }
+
+
 
 
 }
