62bb8f89dcef83a99a402bebfe970988963b29b2
Christopher Skopnik <christopher.skopnik@MAC20220426.drfz.de>
2023-10-30 09:02:58 +0100

    up

--- R/pick_randomNames.R
+++ R/pick_randomNames.R
@@ -21,10 +21,26 @@ pick_randomNames <- function(n,
                              max_iter = 500,
                              min_name_nchar = 3,
                              avoid_chars = c(" ", "-", "'", ",", ";"),
-                             ...) {
+                             randomNames_args = list(which.names = "first")) {
+
+  if ("which.names" %in% names(randomNames_args)) {
+    if (randomNames_args$which.names == "both") {
+      if ("names.sep" %in% names(randomNames_args)) {
+        sep <- randomNames_args$name.sep
+      } else {
+        sep <- formals(randomNames::randomNames)$name.sep
+      }
+      if (length(intersect(trimws(sep), avoid_chars)) > 0) {
+        collision <- intersect(trimws(sep), avoid_chars)
+        stop("name.sep and avoid_chars are colliding: ", collision, ". Please change name.sep or remove this character from avoid_chars.")
+      }
+    }
+  }
+
   names_to_avoid <- names_to_avoid[which(!is.na(names_to_avoid))]
   names_to_avoid <- trimws(names_to_avoid)
-  names <- trimws(unique(randomNames::randomNames(n = n, ...)))
+  names <- trimws(unique(Gmisc::fastDoCall(randomNames::randomNames, args = c(randomNames_args, list(n = n)))))
+  #names <- trimws(unique(randomNames::randomNames(n = n, ...)))
   names <- names[which(!names %in% names_to_avoid)]
   names <- names[sapply(names, nchar, simplify = T) >= min_name_nchar]
   if (!is.null(avoid_chars)) {
@@ -33,7 +49,8 @@ pick_randomNames <- function(n,
 
   iters <- 0
   while (length(names) < n && iters <= max_iter) {
-    temp <- trimws(unique(randomNames::randomNames(n = n - length(names), ...)))
+    temp <- trimws(unique(Gmisc::fastDoCall(randomNames::randomNames, args = c(randomNames_args, list(n = n - length(names))))))
+    #temp <- trimws(unique(randomNames::randomNames(n = n - length(names), ...)))
     temp <- temp[which(!temp %in% c(names_to_avoid, names))]
     temp <- temp[sapply(temp, nchar, simplify = T) >= min_name_nchar]
     if (!is.null(avoid_chars)) {
